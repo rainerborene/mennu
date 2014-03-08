63d18158000704bf8c252a1b46312000b5bd70fd
Rainer Borene <rainerborene@gmail.com>
2014-03-08 16:20:38 -0300

    Typo fix.

--- app/routes/items.rb
+++ app/routes/items.rb
@@ -7,7 +7,7 @@ module Menu
 
       get '/v1/places/:id/items' do
         @place = Place.find_by_pk_or_slug! params[:id]
-        @categories = @place.recent_items.all
+        @categories = @place.latest_menu.all
         respond_to do |format|
           format.xml  { nokogiri :items }
           format.json { @categories.to_json }
