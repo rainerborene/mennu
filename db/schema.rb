88be5193769e5025c7c11ba059432dc8ce6ce6c0
Rainer Borene <rainerborene@gmail.com>
2014-03-13 22:17:28 -0300

    Update DB schema.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,48 @@
 Sequel.migration do
   change do
+    create_table(:photos, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :description
+      String :image, :null=>false
+      Integer :width, :null=>false
+      Integer :height, :null=>false
+      String :content_type, :null=>false
+      String :attachable_id, :null=>false
+      String :attachable_type, :null=>false
+      DateTime :created_at
+      DateTime :updated_at
+      
+      primary_key [:id]
+      
+      index [:attachable_id, :attachable_type]
+    end
+    
+    create_table(:places, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :name, :null=>false
+      String :slug, :null=>false
+      String :email, :null=>false
+      String :password_digest, :null=>false
+      String :description, :null=>false
+      String :logo
+      String :website
+      String :establishment_types
+      TrueClass :opened_to_public, :default=>true
+      DateTime :expire_at
+      DateTime :created_at
+      DateTime :updated_at
+      DateTime :last_publication
+      
+      primary_key [:id]
+      
+      index [:email], :unique=>true
+      index [:slug], :unique=>true
+    end
+    
+    create_table(:schema_info) do
+      Integer :version, :default=>0, :null=>false
+    end
+    
     create_table(:spatial_ref_sys) do
       Integer :srid, :null=>false
       String :auth_name, :size=>256
@@ -8,6 +51,88 @@ Sequel.migration do
       String :proj4text, :size=>2048
       
       primary_key [:srid]
+    end
+    
+    create_table(:users, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :uid, :null=>false
+      String :name
+      String :email, :null=>false
+      String :slug
+      String :secret
+      String :token
+      DateTime :created_at
+      DateTime :updated_at
+      
+      primary_key [:id]
+      
+      index [:slug], :unique=>true
+      index [:uid], :unique=>true
+    end
+    
+    create_table(:addresses, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :street, :null=>false
+      String :street_number, :null=>false
+      String :neighborhood, :null=>false
+      String :city, :null=>false
+      String :state, :null=>false
+      String :postal_code, :null=>false
+      String :geolocation, :null=>false
+      String :phone, :null=>false
+      foreign_key :place_id, :places, :type=>String, :key=>[:id]
+      DateTime :created_at
+      DateTime :updated_at
+      
+      primary_key [:id]
+      
+      index [:place_id]
+    end
+    
+    create_table(:business_hours, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      Integer :weekday, :null=>false
+      Time :start_time, :only_time=>true, :null=>false
+      String :end_time, :null=>false
+      foreign_key :place_id, :places, :type=>String, :key=>[:id]
+      DateTime :created_at
+      DateTime :updated_at
+      
+      primary_key [:id]
+      
+      index [:place_id]
+    end
+    
+    create_table(:categories, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :name, :null=>false
+      String :slug, :null=>false
+      foreign_key :place_id, :places, :type=>String, :key=>[:id]
+      
+      primary_key [:id]
+      
+      index [:place_id]
+    end
+    
+    create_table(:items, :ignore_index_errors=>true) do
+      String :id, :null=>false
+      String :name, :null=>false
+      String :description
+      TrueClass :show_price, :default=>true
+      Float :price
+      Integer :code
+      Integer :position
+      TrueClass :self_service, :default=>false
+      foreign_key :category_id, :categories, :type=>String, :key=>[:id]
+      foreign_key :place_id, :places, :type=>String, :key=>[:id]
+      DateTime :published_at
+      DateTime :created_at
+      DateTime :updated_at
+      
+      primary_key [:id]
+      
+      index [:category_id]
+      index [:place_id]
     end
   end
 end
--- db/schema.sql
+++ db/schema.sql
@@ -24,6 +24,20 @@ COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
 --
+-- Name: citext; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION citext; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION citext IS 'data type for case-insensitive character strings';
+
+
+--
 -- Name: postgis; Type: EXTENSION; Schema: -; Owner: 
 --
 
@@ -49,6 +63,328 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 --
 
 COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: addresses; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE addresses (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    street citext NOT NULL,
+    street_number citext NOT NULL,
+    neighborhood citext NOT NULL,
+    city citext NOT NULL,
+    state citext NOT NULL,
+    postal_code citext NOT NULL,
+    geolocation geometry(Point) NOT NULL,
+    phone citext NOT NULL,
+    place_id uuid,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.addresses OWNER TO rainerborene;
+
+--
+-- Name: business_hours; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE business_hours (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    weekday integer NOT NULL,
+    start_time time without time zone NOT NULL,
+    end_time interval NOT NULL,
+    place_id uuid,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.business_hours OWNER TO rainerborene;
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE categories (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    name citext NOT NULL,
+    slug citext NOT NULL,
+    place_id uuid
+);
+
+
+ALTER TABLE public.categories OWNER TO rainerborene;
+
+--
+-- Name: items; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE items (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    name citext NOT NULL,
+    description citext,
+    show_price boolean DEFAULT true,
+    price double precision,
+    code integer,
+    "position" integer,
+    self_service boolean DEFAULT false,
+    category_id uuid,
+    place_id uuid,
+    published_at timestamp without time zone,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.items OWNER TO rainerborene;
+
+--
+-- Name: photos; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE photos (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    description citext,
+    image citext NOT NULL,
+    width integer NOT NULL,
+    height integer NOT NULL,
+    content_type citext NOT NULL,
+    attachable_id uuid NOT NULL,
+    attachable_type citext NOT NULL,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.photos OWNER TO rainerborene;
+
+--
+-- Name: places; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE places (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    name citext NOT NULL,
+    slug citext NOT NULL,
+    email citext NOT NULL,
+    password_digest citext NOT NULL,
+    description citext NOT NULL,
+    logo citext,
+    website citext,
+    establishment_types citext[],
+    opened_to_public boolean DEFAULT true,
+    expire_at timestamp without time zone,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone,
+    last_publication timestamp without time zone
+);
+
+
+ALTER TABLE public.places OWNER TO rainerborene;
+
+--
+-- Name: schema_info; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE schema_info (
+    version integer DEFAULT 0 NOT NULL
+);
+
+
+ALTER TABLE public.schema_info OWNER TO rainerborene;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE TABLE users (
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    uid citext NOT NULL,
+    name citext,
+    email citext NOT NULL,
+    slug citext,
+    secret citext,
+    token citext,
+    created_at timestamp without time zone,
+    updated_at timestamp without time zone
+);
+
+
+ALTER TABLE public.users OWNER TO rainerborene;
+
+--
+-- Name: addresses_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY addresses
+    ADD CONSTRAINT addresses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: business_hours_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY business_hours
+    ADD CONSTRAINT business_hours_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: categories_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: items_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY items
+    ADD CONSTRAINT items_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: photos_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY photos
+    ADD CONSTRAINT photos_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: places_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY places
+    ADD CONSTRAINT places_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: addresses_place_id_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX addresses_place_id_index ON addresses USING btree (place_id);
+
+
+--
+-- Name: business_hours_place_id_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX business_hours_place_id_index ON business_hours USING btree (place_id);
+
+
+--
+-- Name: categories_place_id_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX categories_place_id_index ON categories USING btree (place_id);
+
+
+--
+-- Name: items_category_id_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX items_category_id_index ON items USING btree (category_id);
+
+
+--
+-- Name: items_place_id_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX items_place_id_index ON items USING btree (place_id);
+
+
+--
+-- Name: photos_attachable_id_attachable_type_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE INDEX photos_attachable_id_attachable_type_index ON photos USING btree (attachable_id, attachable_type);
+
+
+--
+-- Name: places_email_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE UNIQUE INDEX places_email_index ON places USING btree (email);
+
+
+--
+-- Name: places_slug_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE UNIQUE INDEX places_slug_index ON places USING btree (slug);
+
+
+--
+-- Name: users_slug_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE UNIQUE INDEX users_slug_index ON users USING btree (slug);
+
+
+--
+-- Name: users_uid_index; Type: INDEX; Schema: public; Owner: rainerborene; Tablespace: 
+--
+
+CREATE UNIQUE INDEX users_uid_index ON users USING btree (uid);
+
+
+--
+-- Name: addresses_place_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rainerborene
+--
+
+ALTER TABLE ONLY addresses
+    ADD CONSTRAINT addresses_place_id_fkey FOREIGN KEY (place_id) REFERENCES places(id);
+
+
+--
+-- Name: business_hours_place_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rainerborene
+--
+
+ALTER TABLE ONLY business_hours
+    ADD CONSTRAINT business_hours_place_id_fkey FOREIGN KEY (place_id) REFERENCES places(id);
+
+
+--
+-- Name: categories_place_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rainerborene
+--
+
+ALTER TABLE ONLY categories
+    ADD CONSTRAINT categories_place_id_fkey FOREIGN KEY (place_id) REFERENCES places(id);
+
+
+--
+-- Name: items_category_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rainerborene
+--
+
+ALTER TABLE ONLY items
+    ADD CONSTRAINT items_category_id_fkey FOREIGN KEY (category_id) REFERENCES categories(id);
+
+
+--
+-- Name: items_place_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: rainerborene
+--
+
+ALTER TABLE ONLY items
+    ADD CONSTRAINT items_place_id_fkey FOREIGN KEY (place_id) REFERENCES places(id);
 
 
 --
